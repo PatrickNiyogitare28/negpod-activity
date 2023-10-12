57d1c63066eb08d90fb177a2d83fa0d5186fd980
N-Ghi <n.iyankijij@alustudent.com>
2023-10-12 03:56:45 -0700

    ok

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+students_file="students-list_0923.txt"
+
+while true; do
+    echo "Welcome to the ALU Student Registration System"
+    echo "1. Create student record"
+    echo "2. View all students"
+    echo "3. Delete student record"
+    echo "4. Update student record"
+    echo "5. Exit"
+    read -p "Please enter your choice (1/2/3/4/5): " choice
+
