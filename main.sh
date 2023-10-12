c463906c604793d2f89bfb7492044012ba17e97e
N-Ghi <n.iyankijij@alustudent.com>
2023-10-12 06:47:11 -0700

    N-Ghi 6

--- main.sh
+++ main.sh
@@ -51,18 +51,27 @@ case $choice in
 	     4)
 	    read -p "Enter ID to update: " update_id
 	     if grep -q ", $update_id$" "$students_file"; then
-		    read -p "Choose what to update (1 for email, 2 for age): " update_choice"
+		    read -p "Choose what to update (1 for email, 2 for age): " update_choice
 		    case $update_choice in
             1)
                 read -p "Enter updated email: " new_email
+ if [[ $new_email =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+sed -i "s/^[^,]*, [^,]*, $update_id$/$new_email, &/" "$students_file"
+echo "Student email updated"
+else
+echo "Invalid email format. Try Again"
+fi
 		;;
 	2)
                 read -p "Enter updated age: " new_age
+sed -i "s/, $update_id$/, $new_age, $update_id/" "$students_file"
+                    echo "Age updated."
 		;;
+	 *)
+		echo "Invalid Choice"
 esac
-                sed -i "s/.*$update_id$/$new_email, $new_age, $update_id/" "$students_file"
-                echo "Student record updated."
-            else
+
+           else
                 echo "Student with ID $update_id not found."
             fi
             ;;
