aabce05c690cae5135c3314073d34eb1ac3c8084
ngabo-dev <j.niyongabo@alustudent.com>
2023-10-12 05:54:01 -0700

    ngabo-dev

--- select-emails.sh
+++ select-emails.sh
@@ -4,5 +4,9 @@ students_file="students-list_0923.txt"
 #creating the file student-emails.txt
 output_file="student-emails.txt"
 # Extract emails from students file and save them to student-emails.txt
+if [ -e "$students_file" ] && [ -s "$students_file" ]; then
 cut -d ',' -f 1 "$students_file" > "$output_file"
+else
+    echo "Error: Input file does not exist or is empty."
+fi
 echo "Emails extracted and saved to $output_file."
