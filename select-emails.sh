65616e31129788cc5bca47309af9d39907dab517
ykwizera <y.kwizera@alustudent.com>
2023-10-12 05:36:47 -0700

    select

--- select-emails.sh
+++ select-emails.sh
@@ -3,3 +3,4 @@ students_file="students-list_0923.txt"
 output_file="student-emails.txt"
 # Extract emails from students file and save them to student-emails.txt
 cut -d ',' -f 1 "$students_file" > "$output_file"
+echo "Emails extracted and saved to $output_file."
