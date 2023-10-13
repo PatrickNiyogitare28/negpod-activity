87a5bf4f2ae14cbe9330a8f34b33455e223adf5d
aineza2 <a.ineza2@alustudent.com>
2023-10-13 05:20:17 -0700

    done

---
+++ backup-Negpod_7.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#This is a bash script to backup our application on remote server
+
+#Define the remote server
+remote_server="2f05c1f8800b@2f05c1f8800b.3be8ebfc.alu-cod.online"
+remote_dir="/summative/0923-2023S/"
